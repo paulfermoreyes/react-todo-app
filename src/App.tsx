13a999a09b6832a9e1fc99b50159fc264a53c453
paulfermoreyes <paulfermoreyes@gmail.com>
2021-10-13 07:14:06 +0000

    Deploying to gh-pages from @ paulfermoreyes/react-todo-app@0855e5bed170081acfd64a59c2af0eb206b3d052 🚀

---
+++ src/App.tsx
@@ -0,0 +1,66 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Card, Col, Row } from 'react-bootstrap';
+import React from 'react';
+import { IndexKind } from 'typescript';
+import FormTodo from './components/Todo/Form';
+import Todo from './components/Todo/Todo';
+
+function App() {
+  const [todos, setTodos] = React.useState([
+    {
+      text: "This is a sample todo",
+      isDone: false
+    }
+  ])
+
+  const addTodo = (text: String) => {
+    const newTodo = {
+      text: "text",
+      isDone: false
+    }
+    const newTodos = [...todos, newTodo];
+    setTodos(newTodos);
+  }
+
+  const markTodo = (index: IndexKind) => {
+    var newTodos = [...todos];
+    newTodos[index].isDone = !newTodos[index].isDone;
+    // newTodos = newTodos.sort((todoA, todoB) => todoA.isDone === true && todoB.isDone === false ? 1 : -1)
+    setTodos(newTodos);
+  }
+
+  const removeTodo = (index: IndexKind) => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  }
+
+  return (
+    <div className="app">
+      <div className="container">
+        <h1 className="text-center mb-4">Todo List</h1>
+        <FormTodo addTodo={addTodo} />
+        {todos.map((todo, index) => (
+          <Row>
+            <Col>
+              <Card>
+                <Card.Body>
+                  <Todo
+                    key={index}
+                    index={index}
+                    todo={todo}
+                    markTodo={markTodo}
+                    removeTodo={removeTodo}
+                  />
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default App;
---
+++ src/components/Todo/types.ts
@@ -0,0 +1,17 @@
+import { IndexKind } from "typescript"
+
+export type ITodo = {
+    text: String;
+    isDone: Boolean;
+}
+
+export type ITodoProps = {
+    todo: ITodo;
+    index: IndexKind;
+    markTodo: (i: IndexKind) => void;
+    removeTodo: (i: IndexKind) => void;
+}
+/*  */
+export type ITodoFormProps = {
+    addTodo: (s: String) => void;
+}
